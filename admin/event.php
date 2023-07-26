eda39c9bd7aa2fcdc03145a635a5ce5fecbfc479
zeppyx13 <gungnands@gmail.com>
2023-07-26 13:15:24 +0800

    add topup fitur

--- admin/event.php
+++ admin/event.php
@@ -107,6 +107,14 @@ $fbiaya = number_format($lokasi['Biaya'], 0, ',', '.');
             <span class="nav-link-text ms-1">Event</span>
           </a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link text-white " href="./topup.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">diamond</i>
+            </div>
+            <span class="nav-link-text ms-1">Top Up</span>
+          </a>
+        </li>
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
         </li>
--- admin/profile.php
+++ admin/profile.php
@@ -82,6 +82,14 @@ $season = query("SELECT * FROM deadline ORDER BY season DESC")[0];
             <span class="nav-link-text ms-1">Event</span>
           </a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link text-white " href="./topup.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">diamond</i>
+            </div>
+            <span class="nav-link-text ms-1">Top Up</span>
+          </a>
+        </li>
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
         </li>
